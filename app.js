46ac9f30df052d573feae01331fee866d9deb4cf
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-09 21:26:23 +0800

    change data, build function pay later

--- app.js
+++ app.js
@@ -79,7 +79,10 @@ class App extends React.Component {
 
     // toggle class later
     togglePayLater = (event) => {
-
+        const td = event.currentTarget.parentNode;
+        const tr = td.parentNode;
+        tr.classList.add('gray');
+        console.log(tr);
     }
     //   render
     render() {
@@ -173,7 +176,7 @@ class App extends React.Component {
                                                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                                         </svg></button>
-                                                        <button className="btn btn-outline-secondary">Later</button>
+                                                        <button onClick={this.togglePayLater} className="btn btn-outline-secondary">Later</button>
 
                                                     </td>
                                                 </tr>
---
+++ data.js
@@ -0,0 +1,104 @@
+const dad = [
+    {
+        item: 'fresh milk',
+        brand: 'Milo',
+        units: '10 boxes',
+        quantity: 100,
+        isPurchased: false,
+        image: "https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/raw-milk-1-e1563894986431.jpg"
+    },
+    {
+        item: 'hairpins',
+        brand: 'Chanel',
+        units: '2 pieces',
+        quantity: 10,
+        isPurchased: false,
+        image: "https://i.pinimg.com/736x/22/bc/0a/22bc0afd645b57089d62c2a39b432837.jpg"
+    },
+    {
+        item: 'fish sauce',
+        brand: 'knife',
+        units: '3 bottles',
+        quantity: 20,
+        isPurchased: false,
+        image: "https://assets.bonappetit.com/photos/5e6fc9b1b70eca0008656a2b/16:9/w_3488,h_1962,c_limit/Basically-FishSauce.jpg"
+      }
+];
+
+const mom = [
+    {
+        item: 'shoes',
+        brand: 'Gucci',
+        units: '1 pair',
+        quantity: 1,
+        isPurchased: false,
+        image: "https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/53/P00335037.jpg"
+    },
+    {
+        item: 'hairpins',
+        brand: 'Chanel',
+        units: '2 pieces',
+        quantity: 10,
+        isPurchased: false,
+        image: "https://4.bp.blogspot.com/-S3nUDnQS_7k/T1KOMUVeSJI/AAAAAAAABk4/ut06Ja6QJqM/s1600/012.JPG"
+    },
+    {
+        item: 'fish sauce',
+        brand: 'knife',
+        units: '3 bottles',
+        quantity: 20,
+        isPurchased: false,
+        image: "https://assets.bonappetit.com/photos/5e6fc9b1b70eca0008656a2b/16:9/w_3488,h_1962,c_limit/Basically-FishSauce.jpg"
+      }
+];
+
+const son = [
+    {
+        item: 'shirt',
+        brand: 'Uniqlo',
+        units: '2 items',
+        quantity: 2,
+        image: "https://d15udtvdbbfasl.cloudfront.net/catalog/product/large_image/61_127661.jpg"
+    },
+    {
+        item: 'shoes',
+        brand: 'Nike',
+        units: '2 items',
+        quantity: 4,
+        image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1588107378-turb2-1587830727.jpg?crop=1.00xw:0.797xh;0,0.203xh&resize=320%3A%2A"
+    },
+    {
+        item: 'scooter',
+        brand: "no brand",
+        units: '1',
+        quantity: 1,
+        image: "https://images-na.ssl-images-amazon.com/images/I/41xC-OiZjQL._AC_.jpg"
+      }
+];
+
+const daughter = [
+    {
+        item: 'shoes',
+        brand: 'Chanel',
+        units: '1 pair',
+        quantity: 1,
+        image: "https://www.bragmybag.com/wp-content/uploads/2020/03/Chanel-Shoes-Collection-thumb.jpg"
+    },
+    {
+        item: 'hat',
+        brand: 'cotton on',
+        units: '1',
+        quantity: 4,
+        image: "https://media.karousell.com/media/photos/products/2019/05/05/cotton_on_beach_hat_1557047376_6d1ea939_progressive.jpg"
+    },
+    {
+        item: 'earrings',
+        brand: 'PNJ',
+        units: '1 pair',
+        quantity: 10,
+        image: "https://asset.swarovski.com/images/$size_1450/t_swa103/b_rgb:fafafa,c_scale,dpr_3.0,f_auto,w_500/5497872_png/nice-hoop-pierced-earrings--white--rose-gold-tone-plated-swarovski-5497872.png"
+      }
+]
+
+
+
